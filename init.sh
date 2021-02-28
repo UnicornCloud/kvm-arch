330a069c49cda025d8d66684ee12eb0815992788
Pegasus <pegasus@unisocial.net>
2021-02-27 20:59:22 -0500

    hwd

--- init.sh
+++ init.sh
@@ -1,1 +1,13 @@
-ln -srf ../hwd-vfio ./hwd/
+git_submodule(){
+  cd ..
+  git clone https://github.com/UnicornCloud/hwd-vfio.git
+  cd -
+  ln -srf ../hwd-vfio ./hwd/
+}
+
+my_example(){
+  ln -srf my/my.e my/my
+}
+
+git_submodule
+my_example
